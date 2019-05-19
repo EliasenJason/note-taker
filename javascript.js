9ab7eb5db337dda42a7c9703fed659c47ea86005
JasonEliasenHomePC <eliasenjason@gmail.com>
2019-05-18 23:02:26 -0600

    now able to submit popup window and also creates unit list on left

--- javascript.js
+++ javascript.js
@@ -1,16 +1,45 @@
-const popUpActive = false;
-//buttons on main page
-const openPopUpButton = document.querySelector('#issueButton');
-//button within popUp
-const popUpCloseButton = document.querySelector('.fa-times');
+let issues = [];
 
+const reloadUnitList = () => {
+  document.querySelector("#unitList").innerHTML = "";
+  issues.forEach(function(element) {
+    const unit = element.unitNumber;
+    const liNodeToAdd = document.createElement("li");
+    const textNodeToAdd = document.createTextNode(unit);
+    liNodeToAdd.appendChild(textNodeToAdd);
+    liNodeToAdd.classList.add("list-item");
+    console.log(liNodeToAdd);
+    document.querySelector("#unitList").appendChild(liNodeToAdd);
+  })
+}
+
+//create new issue button on main page
+const openPopUpButton = document.querySelector('#issueButton');
 //button to open up the popUp
 openPopUpButton.addEventListener("click", function() {
-  if (!popUpActive) {
     document.querySelector('#popUp').style.display = 'block';
-  };
+    console.log(issues);
 })
+
+//close button within popUp
+const popUpCloseButton = document.querySelector('.fa-times');
 //button to close the popup if you don't want to fill it out
 popUpCloseButton.addEventListener("click", function () {
   document.querySelector('#popUp').style.display = 'none';
+})
+
+//submit button on popup
+const form = document.querySelector('form');
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  const issueObject = {
+    unitNumber: document.querySelector('#unit').value,
+    date: document.querySelector('#date').value,
+    department: document.querySelector('#department').value,
+    issueText: document.querySelector('#issueText').value,
+  };
+  issues.push(issueObject);
+  document.querySelector('#popUp').style.display = 'none';
+  reloadUnitList();
 })
